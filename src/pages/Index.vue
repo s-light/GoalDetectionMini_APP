43a2f419806b261fc08e74714a122f5ad0ee6b03
s-light <git@s-light.eu>
2022-01-18 12:48:19 +0100

    better layout & added reset button

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,18 +6,74 @@
     style="width: 200px; height: 200px"
     > -->
     <section>
-      <p>
-        connected: {{ connected }}
-      </p>
-      <p>
-        goal1: {{ goal1 }}
-      </p>
-      <p>
-        goal2: {{ goal2 }}
-      </p>
+        <div class="top_line">
+            <button
+                type="button" name="reset" v-on:click="goal1=0; goal2=0;"
+            >
+                Reset
+            </button>
+            connected: {{ connected }}
+        </div>
+        <div class="goal_info">
+            <div>
+                <p>goal1</p>
+                <h1>{{ goal1 }}</h1>
+            </div>
+            <div>
+                <p>goal2</p>
+                <h1>{{ goal2 }}</h1>
+            </div>
+        </div>
     </section>
   </q-page>
 </template>
+
+<style lang="css">
+section {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-content: center;
+    align-items: stretch;
+    width: 100%;
+    height: 100%;
+}
+
+section .top_line,
+section .goal_info {
+    order: 0;
+    flex: 0 1 auto;
+    align-self: auto;
+}
+
+section .top_line {
+    /* background-color: hsl(200, 100%, 50%); */
+    text-align: center;
+}
+
+.goal_info {
+    flex: 1 1 auto;
+}
+
+.goal_info {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-content: center;
+    align-items: stretch;
+}
+
+.goal_info > div {
+    order: 0;
+    flex: 0 1 auto;
+    align-self: auto;
+    margin: 0 1em;
+    text-align: center;
+}
+
+</style>
 
 <script>
 import {
